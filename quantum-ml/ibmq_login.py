1c4e53128c3a783b47cab2a0f4272275c8576755
Mizuki <125197625+mzk8888@users.noreply.github.com>
2025-10-05 16:15:15 +0900

    Update ibmq_login.py

--- quantum-ml/ibmq_login.py
+++ quantum-ml/ibmq_login.py
@@ -3,10 +3,11 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 # IBM Quantum に接続
 service = QiskitRuntimeService(
     channel="ibm_quantum_platform",
-    token="B3QJtC4quW4P-coUN7SMN5K6l6G3jrjKaA-ax1iVjxZw",   # setx で保存してあるなら省略可能
+    token="API token",   # setx で保存してあるなら省略可能
     instance="my-quantum-instance"  # ここが大事！
 )
 
 # 利用可能なバックエンド一覧を表示
 print(service.backends())
+
 
